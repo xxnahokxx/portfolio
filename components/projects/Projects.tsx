cf1815c1abd81ffe20b667def805aa28b5f3fb23
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-07-29 22:07:11 -0500

    feat(projects): add projects render

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,10 +1,15 @@
 
 import React from 'react'
+import Card from './Card'
+import { projects } from '@/data'
+// import image from "@/public/javaScript.svg"
 
 const Projects = () => {
   return (
       <div className="" id="projects">
-          Project
+      <h2>PORTFOLIO</h2>
+      <p>Te presento a continuacion algunos de los proyectos en los que e participado.</p>
+      {projects?.map((el, index) => <Card key={index} title={el.title} description={el.description} image={el.image} TechStack={el.TechStack} page={el.page} code={el.code} direction={index % 2 === 0 ? false : true} />)}
     </div>
   )
 }
---
+++ data.ts
@@ -0,0 +1,21 @@
+
+const projects = [
+    {
+        title: "Parcels",
+        description: "Proyecto final Henry: Nuestro proyecto consiste en una plataforma de e- commerce especializada en la venta de parcelas, brindando a nuestros usuarios la oportunidad de encontrar el terreno perfecto para sus proyectos o inversiones. Utilizamos una sólida pila tecnológica que incluye NextJs, Redux Toolkit, TypeScript, MongoDB, Mongoose Cloudinary, Api Google Maps.",
+        image: "https://parcelas.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FhomeLogo.729be7b9.png&w=1920&q=75",
+        TechStack: ["Next.js","MongoDB","Cloudinary","Api Google maps", "TypeScript", "Redux Toolkit", "Tailwind"],
+        page: "https://parcelas.vercel.app/",
+        code: "https://github.com/LucasRibotta/Project-Parcels",
+    },
+    {
+        title: "Doggy Style",
+        description: "Proyecto individual de Henry: Este proyecto mas que nada esta creado con la intencion de tener una pagina en la cual se pueda consultar las razas de perro existentes, y poder ver al detalle cada una de estas, y en caso de que no exista una en nuestro catalogo, podemos agregar una nueva raza con nuestro formulario de creacion.",
+        image: "https://svgsilh.com/svg/1293792.svg",
+        TechStack: ["React","javascript","express","postgreSQL", "CSS"],
+        page: "https://doggystyle.up.railway.app/",
+        code: "https://github.com/xxnahokxx/pi_dogs_henry",
+    },
+]
+
+export {projects}
