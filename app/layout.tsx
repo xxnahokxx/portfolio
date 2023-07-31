c3f2437a58dbd844db843976996b76eb23f723fa
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-07-30 20:51:02 -0500

    feat(layout): scroll-smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='scroll-smooth'>
       <head>
         <link rel="icon" href="/ico.svg" />
       </head>
