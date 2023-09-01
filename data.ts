5316dc0e8eff21cb25aeaa8a7ac51fcf9768c4ba
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-08-31 21:48:05 -0500

    feat(): projects

--- data.ts
+++ data.ts
@@ -1,21 +1,30 @@
 
 const projects = [
     {
-        title: "Parcels",
-        description: "Proyecto final Henry: Nuestro proyecto consiste en una plataforma de e- commerce especializada en la venta de parcelas, brindando a nuestros usuarios la oportunidad de encontrar el terreno perfecto para sus proyectos o inversiones. Utilizamos una sólida pila tecnológica que incluye NextJs, Redux Toolkit, TypeScript, MongoDB, Mongoose Cloudinary, Api Google Maps.",
-        image: "https://parcelas.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FhomeLogo.729be7b9.png&w=1920&q=75",
-        TechStack: ["Next.js","MongoDB","Cloudinary","Api Google maps", "TypeScript", "Redux Toolkit", "Tailwind"],
-        page: "https://parcelas.vercel.app/",
-        code: "https://github.com/LucasRibotta/Project-Parcels",
-    },
-    {
         title: "Doggy Style",
         description: "Proyecto individual de Henry: Este proyecto mas que nada esta creado con la intencion de tener una pagina en la cual se pueda consultar las razas de perro existentes, y poder ver al detalle cada una de estas, y en caso de que no exista una en nuestro catalogo, podemos agregar una nueva raza con nuestro formulario de creacion.",
         image: "https://svgsilh.com/svg/1293792.svg",
-        TechStack: ["React","javascript","express","postgreSQL", "CSS"],
+        TechStack: ["React", "javascript", "express", "postgreSQL", "CSS"],
         page: "https://doggystyle.up.railway.app/",
         code: "https://github.com/xxnahokxx/pi_dogs_henry",
     },
+    {
+        title: "Parcels",
+        description: "Proyecto final Henry: Nuestro proyecto consiste en una plataforma de e- commerce especializada en la venta de parcelas, brindando a nuestros usuarios la oportunidad de encontrar el terreno perfecto para sus proyectos o inversiones. Utilizamos una sólida pila tecnológica que incluye NextJs, Redux Toolkit, TypeScript, MongoDB, Mongoose Cloudinary, Api Google Maps.",
+        image: "https://parcelas.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FhomeLogo.729be7b9.png&w=1920&q=75",
+        TechStack: ["Next.js", "MongoDB", "Cloudinary", "Api Google maps", "TypeScript", "Redux Toolkit", "Tailwind"],
+        page: "https://parcelas.vercel.app/",
+        code: "https://github.com/LucasRibotta/Project-Parcels",
+    },
+
+    {
+        title: "Rick and Morty",
+        description: "Para este proyecto, despues del conocimiento adquirido con el proyecto grupal, decidí acentar mis conocimientos en las tecnologias adiquiridas en la fase grupal y generar esta pagina con la cual se busca aprovechar todos esos veneficios que nos birnda la API de Rick and Morty.  Este proyecto esta en proceso de desarrollo...",
+        image: "https://rick-and-morty-johan.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FRaM1.ce9fedc2.png&w=828&q=75",
+        TechStack: ["Next.js", "TypeScript", "Redux Toolkit", "Mongoose", "MongoDB", "Tailwind", "Clerk"],
+        page: "https://rick-and-morty-johan.vercel.app/",
+        code: "https://github.com/xxnahokxx/rick-and-morty-Next",
+    },
 ]
 
-export {projects}
+export { projects }
