45fb754bb16aa04da812ec414f55b109a40d3949
Johan Amaya <157429821+AmayaJohan@users.noreply.github.com>
2024-05-07 18:12:27 -0500

    update(): navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = ({ theme, status }: { theme: Function, status: string }) => {
     return (
         <div className='flex w-[100%] justify-between px-12 py-6 bg-white dark:bg-slate-900 transition-colors duration-300 fixed drop-shadow-md z-30'>
             <h2 className='font-bold text-[20px] self-center'>
-                Johan.dev
+                NaHoK.dev
             </h2>
             <div className='flex gap-4 font-semibold self-center'>
                 <a href="#home">Inicio</a>
